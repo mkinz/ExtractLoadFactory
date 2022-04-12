f88da81bc3f6aeaa0d015bb9388fb8a20d5fbdd7
mkinz <matthew.kinzler@gmail.com>
2022-04-12 14:15:28 -0400

    add loader module

---
+++ loader.py
@@ -0,0 +1,22 @@
+import logging
+from logging import Logger
+from typing import Protocol
+from datetime import datetime
+from dbcon import DBConnector
+
+logger: Logger = logging.getLogger(__name__)
+
+class Loader(Protocol):
+    def load_data(self, target_db_connection: DBConnector, data_to_load, date_time: datetime):
+        pass
+
+
+class ConcreteLoaderInstanceA:
+    def load_data(self, target_db_connection: DBConnector, data_to_load, date_time: datetime):
+        query = f"update {target_db_connection}.myschma.mytable" \
+                f"set data = {data_to_load}" \
+                f"where date = {date_time}"
+        logger.debug(f"Ran the following SQL:")
+        logger.debug(query)
+        logger.debug(f"Load sql query complete to {target_db_connection}.myschema.mytable.")
+        return target_db_connection
