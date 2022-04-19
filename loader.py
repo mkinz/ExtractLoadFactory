60d904ba4166555a68873d0f83bea4a2190af7be
mkinz <matthew.kinzler@gmail.com>
2022-04-19 12:22:13 -0400

    update loader interface with new method

--- loader.py
+++ loader.py
@@ -1,4 +1,5 @@
 import logging
+import pandas as pd
 from logging import Logger
 from typing import Protocol, runtime_checkable
 from datetime import datetime
@@ -9,26 +10,54 @@ logger: Logger = logging.getLogger(__name__)
 
 @runtime_checkable
 class Loader(Protocol):
-    def load_data(self, target_db_connection: DBConnector, data_to_load, date_time: datetime):
+    def __init__(self):
+        ...
+
+    def validate_input(self, data_to_load: pd.DataFrame):
+        ...
+
+
+    def load_data(self, target_db_connection: DBConnector, data_to_load: pd.DataFrame, date_time: datetime):
         ...
 
 
 class ConcreteLoaderInstanceA:
-    def load_data(self, target_db_connection: DBConnector, data_to_load, date_time: datetime):
+
+    def  __init__(self):
+        self.contains_data = False
+
+    def validate_input(self, data_to_load):
+        if not data_to_load.empty:
+            self.contains_data = True
+
+    def load_data(self, target_db_connection: DBConnector, data_to_load: pd.DataFrame, date_time: datetime):
         query = f"update {target_db_connection}.myschma.mytable_A" \
                 f"set data = {data_to_load}" \
                 f"where date = {date_time}"
-        logger.debug(f"Ran the following SQL query:")
-        logger.debug(query)
-        logger.debug(f"Load sql query complete to {target_db_connection}.myschema.mytable_A.")
-        return target_db_connection
+        if self.contains_data:
+            logger.debug(f"Ran the following SQL query:")
+            logger.debug(query)
+            logger.debug(f"Load sql query complete to {target_db_connection}.myschema.mytable_A.")
+            return target_db_connection
+        else:
+            logger.info("No data in input dataframe, so no data loaded.")
 
 class ConcreteLoaderInstanceB:
-    def load_data(self, target_db_connection: DBConnector, data_to_load, date_time: datetime):
+
+    def __init__(self):
+        self.contains_data = False
+
+    def validate_input(self, data_to_load):
+        if not data_to_load.empty:
+            self.contains_data = True
+    def load_data(self, target_db_connection: DBConnector, data_to_load: pd.DataFrame, date_time: datetime):
         query = f"update {target_db_connection}.myschma.mytable_B" \
                 f"set data = {data_to_load}" \
                 f"where date = {date_time}"
-        logger.debug(f"Ran the following SQL query:")
-        logger.debug(query)
-        logger.debug(f"Load sql query complete to {target_db_connection}.myschema.mytable_B.")
-        return target_db_connection
+        if self.contains_data:
+            logger.debug(f"Ran the following SQL query:")
+            logger.debug(query)
+            logger.debug(f"Load sql query complete to {target_db_connection}.myschema.mytable_B.")
+            return target_db_connection
+        else:
+            logger.info("No data in input dataframe, so no data loaded.")
