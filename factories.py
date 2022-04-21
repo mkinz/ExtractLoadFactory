c94c9285694372b4b28c30bf013f463b8963dd7e
mkinz <matthew.kinzler@gmail.com>
2022-04-21 14:27:31 -0400

    add generic data refresher factory class, and set concrete factories as subclass from it

--- factories.py
+++ factories.py
@@ -33,37 +33,37 @@ class DataRefresherFactory(Protocol):
         ...
 
 
-class ConcreteDataRefresherFactoryA:
-    
+class GenericConcreteDataRefresherFactory:
+
     def get_source_connector(self) -> DBConnector:
         return SourceDBConnector()
-    
+
     def get_extractor(self) -> Extractor:
-        return ConcreteExtractorInstanceA()
-    
+        pass
+
     def get_target_connector(self) -> DBConnector:
         return TargetDBConnector()
-    
+
     def get_loader(self) -> Loader:
-        return ConcreteLoaderInstanceA()
+        pass
 
     def get_validator(self) -> IValidator:
         return ConcreteValidator()
 
 
-class ConcreteDataRefresherFactoryB:
+class ConcreteDataRefresherFactoryA(GenericConcreteDataRefresherFactory):
 
-    def get_source_connector(self) -> DBConnector:
-        return SourceDBConnector()
+    def get_extractor(self) -> Extractor:
+        return ConcreteExtractorInstanceA()
+
+    def get_loader(self) -> Loader:
+        return ConcreteLoaderInstanceA()
+
+
+class ConcreteDataRefresherFactoryB(GenericConcreteDataRefresherFactory):
     
     def get_extractor(self) -> Extractor:
         return ConcreteExtractorInstanceB()
 
-    def get_target_connector(self) -> DBConnector:
-        return TargetDBConnector()
-    
     def get_loader(self) -> Loader:
         return ConcreteLoaderInstanceB()
-
-    def get_validator(self) -> IValidator:
-        return ConcreteValidator()
