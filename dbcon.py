6c4e45ad72bc8f9746152d14dcb3b57822262c43
mkinz <matthew.kinzler@gmail.com>
2022-04-21 08:49:08 -0400

    add connection state information to connectors and exceptions to catch connection issues

--- dbcon.py
+++ dbcon.py
@@ -7,6 +7,10 @@ logger: Logger = logging.getLogger(__name__)
 
 @runtime_checkable
 class DBConnector(Protocol):
+
+    def __init__(self):
+        ...
+
     def connect_to_db(self):
         ...
 
@@ -16,7 +20,11 @@ class SourceDBConnector:
     username = 'my_user'
     connection_str = 'my_source_connection_string'
 
+    def __init__(self):
+        self.connected = False
+
     def connect_to_db(self):
+        self.connected = True
         logger.info(f"Connected to {self.dbname} with user {self.username} and"
                     f"connection details '{self.connection_str}'.")
         return self.dbname
@@ -27,7 +35,11 @@ class TargetDBConnector:
     username = 'my_user'
     connection_str = 'my_target_connection_string'
 
+    def __init__(self):
+        self.connected = False
+
     def connect_to_db(self):
+        self.connected = True
         logger.info(f"Connected to {self.dbname} with user {self.username} and"
                     f"connection details '{self.connection_str}'.")
         return self.dbname
--- main.py
+++ main.py
@@ -9,6 +9,21 @@ logging.basicConfig(level=logging.DEBUG)
 logger: Logger = logging.getLogger(__name__)
 
 
+class ConnectionError(Exception):
+    """
+    Custom exception for handling connection errors
+    """
+    pass
+
+
+class SourceConnectionError(ConnectionError):
+    pass
+
+
+class TargetConnectionError(ConnectionError):
+    pass
+
+
 def get_factory() -> Iterable:
     factories = {
         "A": ConcreteDataRefresherFactoryA(),
@@ -18,25 +33,34 @@ def get_factory() -> Iterable:
     for work_set in factories.keys():
         yield factories[work_set]
 
+
 def run_app():
     logger.info("Initializing ExtractLoadFactory...")
     today = datetime.today()
 
     for fac in get_factory():
-        source_db_connector_object = fac.get_source_connector()
-        source_conn = source_db_connector_object.connect_to_db()
+        try:
+            source_db_connector_object = fac.get_source_connector()
+            source_conn = source_db_connector_object.connect_to_db()
+            if not source_db_connector_object.connected:
+                raise SourceConnectionError
 
-        extractor_object = fac.get_extractor()
-        extracted_data = extractor_object.extract(source_conn, today)
+            extractor_object = fac.get_extractor()
+            extracted_data = extractor_object.extract(source_conn, today)
 
-        target_db_connector_object = fac.get_target_connector()
-        target_conn = target_db_connector_object.connect_to_db()
+            target_db_connector_object = fac.get_target_connector()
+            target_conn = target_db_connector_object.connect_to_db()
+            if not target_db_connector_object.connected:
+                raise TargetConnectionError
 
-        loader_object = fac.get_loader()
-        loader_object.validate_input(extracted_data)
-        loader_object.load_data(target_conn, extracted_data, today)
+            loader_object = fac.get_loader()
+            loader_object.validate_input(extracted_data)
+            loader_object.load_data(target_conn, extracted_data, today)
+        except ConnectionError:
+            logger.info("Connection was unabled to be established.")
 
         logger.info("Extract and Load complete.\n")
+
 
 def main():
     run_app()
